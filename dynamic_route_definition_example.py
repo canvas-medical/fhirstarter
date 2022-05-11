88d01f090958c2781d2b04adebcd54b907f61573
Christopher Sande <christopher.sande@canvasmedical.com>
2022-05-11 17:21:51 -0500

    Added lots of comments and an example that shows how to use imports

--- dynamic_route_definition_example.py
+++ dynamic_route_definition_example.py
@@ -3,29 +3,45 @@ from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 from types import CodeType, FunctionType
 import uvicorn
+from uuid import uuid4
 
 
 def _add_route(app, path, message):
+    # Define the function (annotations, defaults, and imports not needed as they are defined elsewhere
     test_code_str = f"""
 def test(int_val, str_val):
     return {{
         "message": "{message}",
         "i": int_val,
-        "s": str_val
+        "s": str_val,
+        "id": f"{{uuid4()}}"
     }}
 """
 
-    test_compiled = compile(test_code_str, '<string>', 'exec')
-    test_code = [c for c in test_compiled.co_consts if isinstance(c, CodeType)][0]
-    arg_defaults = ('abc',)  # counts backwards, so only str_val gets a default
-    test_func = FunctionType(test_code, globals(), "test", arg_defaults)
-    test_func.__annotations__ = {"int_val": int, "str_val": str}
+    # Compile the code
+    test_compiled = compile(test_code_str, "<string>", "exec")
 
+    # Find the code object that represents the function (this only works if there is exactly one function defined in the code, which will be the case)
+    test_code = next(c for c in test_compiled.co_consts if isinstance(c, CodeType))
+
+    # Define the globals, including imports
+    globals_ = {"uuid4": uuid4}
+
+    # Define the function defaults
+    arg_defaults = ("abc",)  # counts backwards, so only str_val gets a default
+
+    # Create the callable
+    test_func = FunctionType(test_code, globals_, "test", arg_defaults)
+
+    # Annotate the function arguments
+    test_func.__annotations__ = {"int_val": int, "str_val": str, "return": dict[str, Any]}
+
+    # Create the API route
     app.get(path, response_model=dict[str, Any])(test_func)
 
 
 app = FastAPI()
-_add_route(app, '/test', 'Hello World')
+_add_route(app, "/test", "Hello World")
 
 
 @app.get("/", include_in_schema=False)
@@ -34,5 +50,5 @@ async def index() -> RedirectResponse:
     return RedirectResponse("/docs")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(app)
