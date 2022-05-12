a72837fbaf34777f2ab09d8c6469bf02fef03f64
Christopher Sande <christopher.sande@canvasmedical.com>
2022-05-12 10:43:35 -0500

    Minor cleanup

--- dynamic_route_definition_example.py
+++ dynamic_route_definition_example.py
@@ -1,13 +1,15 @@
-from typing import Any
-from fastapi import FastAPI
-from starlette.responses import RedirectResponse
 from types import CodeType, FunctionType
-import uvicorn
+from typing import Any
 from uuid import uuid4
 
+import uvicorn
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
-def _add_route(app, path, message):
-    # Define the function (annotations, defaults, and imports not needed as they are defined elsewhere)
+
+def _add_route(app: FastAPI, path: str, message: str) -> None:
+    # Define the function (annotations, defaults, and imports not needed as they are defined
+    # elsewhere)
     test_code_str = f"""
 def test(int_val, str_val):
     return {{
@@ -19,10 +21,11 @@ def test(int_val, str_val):
 """
 
     # Compile the code
-    test_compiled = compile(test_code_str, "<string>", "exec")
+    code = compile(test_code_str, "<string>", "exec")
 
-    # Find the code object that represents the function (this only works if there is exactly one function defined in the code, which will be the case)
-    test_code = next(c for c in test_compiled.co_consts if isinstance(c, CodeType))
+    # Find the code object that represents the function (this only works if there is exactly one
+    # function defined in the code, which will be the case)
+    func_code = next(c for c in code.co_consts if isinstance(c, CodeType))
 
     # Define the globals, including imports
     globals_ = {"uuid4": uuid4}
@@ -30,14 +33,14 @@ def test(int_val, str_val):
     # Define the function argument defaults
     arg_defaults = ("abc",)  # counts backwards, so only str_val gets a default
 
-    # Create the callable
-    test_func = FunctionType(test_code, globals_, "test", arg_defaults)
+    # Create the function
+    func = FunctionType(func_code, globals_, "test", arg_defaults)
 
-    # Annotate the function arguments on the callable
-    test_func.__annotations__ = {"int_val": int, "str_val": str, "return": dict[str, Any]}
+    # Annotate the function arguments on the function
+    func.__annotations__ = {"int_val": int, "str_val": str, "return": dict[str, Any]}
 
     # Create the API route
-    app.get(path, response_model=dict[str, Any])(test_func)
+    app.get(path, response_model=dict[str, Any])(func)
 
 
 app = FastAPI()
