145543b01a70e627452021e80687e172396c35e2
Christopher Sande <christopher.sande@canvasmedical.com>
2022-06-06 16:09:59 -0500

    Added unit tests for create resource, and for validation errors

--- fhirstarter/tests.py
+++ fhirstarter/tests.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from uuid import uuid4
 
 from fhir.resources.patient import Patient
@@ -6,19 +7,30 @@ from . import FHIRInteractionResult, FHIRProvider, FHIRStarter, status
 from .exceptions import FHIRResourceNotFoundError
 from .testclient import TestClient
 
-_ID = uuid4().hex
+_DATABASE: dict[str, Patient] = {}
+
 
 provider = FHIRProvider()
 
 
-@provider.register_read_interaction(Patient)
-async def read(id_: str) -> FHIRInteractionResult[Patient]:
-    if id_ != "found":
-        raise FHIRResourceNotFoundError
+def _generate_patient_id() -> str:
+    return uuid4().hex
 
-    patient = Patient(
-        **{"id": _ID, "name": [{"family": "Baggins", "given": ["Bilbo"]}]}
-    )
+
+@provider.register_create_interaction(Patient)
+async def patient_create(resource: Patient) -> FHIRInteractionResult[Patient]:
+    patient = deepcopy(resource)
+    patient.id = _generate_patient_id()
+    _DATABASE[patient.id] = patient
+
+    return FHIRInteractionResult[Patient](patient.id)
+
+
+@provider.register_read_interaction(Patient)
+async def patient_read(id_: str) -> FHIRInteractionResult[Patient]:
+    patient = _DATABASE.get(id_)
+    if not patient:
+        raise FHIRResourceNotFoundError
 
     return FHIRInteractionResult[Patient](patient.id, patient)
 
@@ -29,18 +41,28 @@ app.add_providers(provider)
 client = TestClient(app)
 
 
-def test_patient_read() -> None:
-    response = client.get("/Patient/found")
+def test_patient_create_and_read() -> None:
+    response = client.post(
+        "/Patient", json={"name": [{"family": "Baggins", "given": ["Bilbo"]}]}
+    )
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    id_ = response.headers["Location"].split("/")[4]
+    response = client.get(f"/Patient/{id_}")
+
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {
         "resourceType": "Patient",
-        "id": _ID,
+        "id": id_,
         "name": [{"family": "Baggins", "given": ["Bilbo"]}],
     }
 
 
 def test_patient_read_not_found() -> None:
-    response = client.get("/Patient/notfound")
+    id_ = _generate_patient_id()
+    response = client.get(f"/Patient/{id_}")
+
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {
         "resourceType": "OperationOutcome",
@@ -48,7 +70,25 @@ def test_patient_read_not_found() -> None:
             {
                 "severity": "error",
                 "code": "not-found",
-                "details": {"text": "Unknown Patient resource 'notfound'"},
+                "details": {"text": f"Unknown Patient resource '{id_}'"},
+            }
+        ],
+    }
+
+
+def test_validation_error() -> None:
+    response = client.post("/Patient", json={"extraField": []})
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {
+        "resourceType": "OperationOutcome",
+        "issue": [
+            {
+                "severity": "fatal",
+                "code": "structure",
+                "details": {
+                    "text": "1 validation error for Request\nbody -> extraField\n  extra fields not permitted (type=value_error.extra)"
+                },
             }
         ],
     }
