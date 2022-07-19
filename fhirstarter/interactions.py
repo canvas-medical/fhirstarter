763b4bc8371920499e3a77c60d1098430ee7c47c
Christopher Sande <christopher.sande@canvasmedical.com>
2022-07-19 11:43:56 -0500

    Updated docstring formatting

--- fhirstarter/interactions.py
+++ fhirstarter/interactions.py
@@ -65,7 +65,7 @@ class TypeInteraction(Generic[ResourceType]):
 
     resource_type:    The type of FHIR resource on which this interaction operates, as defined by
                       the fhir.resources package.
-    handler:        User-defined function that performs the FHIR interaction.
+    handler:          User-defined function that performs the FHIR interaction.
     route_options:    Dictionary of key-value pairs that are passed on to FastAPI on route creation.
     """
 
