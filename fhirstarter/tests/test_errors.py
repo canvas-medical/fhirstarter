f6060a6e6d8314474c1dbdeaae88d9417b47d216
Christopher Sande <christopher.sande@canvasmedical.com>
2022-08-29 11:39:52 -0500

    Added a test for HTTP exception handling

--- fhirstarter/tests/test_errors.py
+++ fhirstarter/tests/test_errors.py
@@ -1,10 +1,15 @@
 """Test FHIRStarter error handling"""
 
-from ..fhirstarter import status
+from fastapi import HTTPException
+from fhir.resources.fhirtypes import Id
+from fhir.resources.patient import Patient
+
+from ..fhirstarter import FHIRProvider, status
+from ..interactions import InteractionContext
 from ..testclient import TestClient
 from ..utils import make_operation_outcome
-from .fixtures import client_fixture
-from .utils import assert_expected_response
+from .fixtures import app, client_fixture
+from .utils import assert_expected_response, generate_fhir_resource_id
 
 
 def test_validation_error(client_fixture: TestClient) -> None:
@@ -24,4 +29,33 @@ def test_validation_error(client_fixture: TestClient) -> None:
             details_text="1 validation error for Request\nbody -> extraField\n  extra fields not "
             "permitted (type=value_error.extra)",
         ).dict(),
+    )
+
+
+def test_http_exception() -> None:
+    """Test exception handling for HTTP Exceptions."""
+
+    async def patient_read(context: InteractionContext, id_: Id) -> Patient:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+    provider = FHIRProvider()
+    provider.read(Patient)(patient_read)
+
+    client = app(provider)
+
+    response = client.get(f"/Patient/{generate_fhir_resource_id()}")
+
+    assert_expected_response(
+        response,
+        status.HTTP_400_BAD_REQUEST,
+        content={
+            "resourceType": "OperationOutcome",
+            "issue": [
+                {
+                    "severity": "error",
+                    "code": "processing",
+                    "details": {"text": "Bad Request"},
+                }
+            ],
+        },
     )
