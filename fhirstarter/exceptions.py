6e28f239ef000a8d8845e5f2094609dfa77b6f5f
Christopher Sande <christopher.sande@canvasmedical.com>
2022-10-21 15:58:39 -0500

    Refactored exception hierarchy to inherit from Starlette's HTTPException

--- fhirstarter/exceptions.py
+++ fhirstarter/exceptions.py
@@ -6,15 +6,15 @@ containing an OperationOutcome and an HTTP status code.
 """
 
 from abc import ABC, abstractmethod
-from typing import Any
 
 from fastapi import Request, status
+from fastapi.exceptions import HTTPException
 from fhir.resources.operationoutcome import OperationOutcome
 
 from .utils import make_operation_outcome
 
 
-class FHIRException(Exception, ABC):
+class FHIRException(HTTPException, ABC):
     """
     Abstract base class for all FHIR exceptions.
 
@@ -22,16 +22,12 @@ class FHIRException(Exception, ABC):
     object for additional context.
     """
 
-    def __init__(self, *args: Any) -> None:
-        super().__init__(*args)
+    def __init__(self, status_code: int, details_text: str | None = None) -> None:
+        super().__init__(status_code, detail=details_text)
         self._request: Request | None = None
 
     def set_request(self, request: Request) -> None:
         self._request = request
-
-    @abstractmethod
-    def status_code(self) -> int:
-        raise NotImplementedError
 
     @abstractmethod
     def operation_outcome(self) -> OperationOutcome:
@@ -46,10 +42,9 @@ class FHIRGeneralError(FHIRException):
     """
 
     def __init__(
-        self, status_code: int, severity: str, code: str, details_text: str, *args: Any
+        self, status_code: int, severity: str, code: str, details_text: str
     ) -> None:
-        super().__init__(*args)
-        self._status_code_ = status_code
+        super().__init__(status_code, details_text)
         self._operation_outcome_ = make_operation_outcome(severity, code, details_text)
 
     @classmethod
@@ -60,9 +55,6 @@ class FHIRGeneralError(FHIRException):
         error._operation_outcome_ = operation_outcome
         return error
 
-    def status_code(self) -> int:
-        return self._status_code_
-
     def operation_outcome(self) -> OperationOutcome:
         return self._operation_outcome_
 
@@ -70,57 +62,45 @@ class FHIRGeneralError(FHIRException):
 class FHIRBadRequestError(FHIRException):
     """FHIR exception class for 400 bad request errors."""
 
-    def __init__(self, code: str, details_text: str, *args: Any) -> None:
-        super().__init__(*args)
+    def __init__(self, code: str, details_text: str) -> None:
+        super().__init__(status.HTTP_400_BAD_REQUEST, details_text)
         self._code = code
-        self._details_text = details_text
-
-    def status_code(self) -> int:
-        return status.HTTP_400_BAD_REQUEST
 
     def operation_outcome(self) -> OperationOutcome:
         return make_operation_outcome(
-            severity="error", code=self._code, details_text=self._details_text
+            severity="error", code=self._code, details_text=self.detail
         )
 
 
 class FHIRUnauthorizedError(FHIRException):
-    """FHIR exception class for authentication errors."""
+    """FHIR exception class for 401 authentication errors."""
 
-    def __init__(self, details_text: str, *args: Any) -> None:
-        super().__init__(*args)
-        self._details_text = details_text
-
-    def status_code(self) -> int:
-        return status.HTTP_401_UNAUTHORIZED
+    def __init__(self, details_text: str) -> None:
+        super().__init__(status.HTTP_401_UNAUTHORIZED, details_text)
 
     def operation_outcome(self) -> OperationOutcome:
         return make_operation_outcome(
-            severity="error", code="unknown", details_text=self._details_text
+            severity="error", code="unknown", details_text=self.detail
         )
 
 
 class FHIRForbiddenError(FHIRException):
     """FHIR exception class for 403 forbidden errors."""
 
-    def __init__(self, details_text: str, *args: Any) -> None:
-        super().__init__(*args)
-        self._details_text = details_text
-
-    def status_code(self) -> int:
-        return status.HTTP_403_FORBIDDEN
+    def __init__(self, details_text: str) -> None:
+        super().__init__(status.HTTP_403_FORBIDDEN, details_text)
 
     def operation_outcome(self) -> OperationOutcome:
         return make_operation_outcome(
-            severity="error", code="forbidden", details_text=self._details_text
+            severity="error", code="forbidden", details_text=self.detail
         )
 
 
 class FHIRResourceNotFoundError(FHIRException):
     """FHIR exception class for 404 not found errors."""
 
-    def status_code(self) -> int:
-        return status.HTTP_404_NOT_FOUND
+    def __init__(self) -> None:
+        super().__init__(status.HTTP_404_NOT_FOUND)
 
     def operation_outcome(self) -> OperationOutcome:
         try:
