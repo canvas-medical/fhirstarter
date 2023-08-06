18637596a0622b18b336111793a097d7724b5bc6
Christopher Sande <christopher.sande@canvasmedical.com>
2023-08-05 22:03:14 -0500

    Added a script that will populate the example directories for each sequence

--- fhirstarter/fhir/utils/download_examples.py
+++ fhirstarter/fhir/utils/download_examples.py
@@ -6,6 +6,28 @@ from pathlib import Path
 
 import requests
 
+FILENAME_EXCEPTIONS: dict[str, dict[str, str]] = {
+    "STU3": {},
+    "R4": {
+        "BodyStructure": "bodystructure-example-skin-patch.json",
+        "ChargeItemDefinition": "chargeitemdefinition-device-example.json",
+        "Measure": "measure-hiv-indicators.json",
+        "MeasureReport": "measurereport-cms146-cat2-example.json",
+        "Medication": "medicationexample15.json",
+        "MedicationAdministration": "medicationadministration0302.json",
+        "MedicationDispense": "medicationdispense0302.json",
+        "MedicationRequest": "medicationrequest0301.json",
+        "MedicationStatement": "medicationstatementexample1.json",
+        "NutritionOrder": "nutritionorder-example-renaldiet.json",
+        "SpecimenDefinition": "specimendefinition-example-serum-plasma.json",
+        "StructureDefinition": "structuredefinition-example-section-library.json",
+        "SupplyRequest": "supplyrequest-example-simpleorder.json",
+        "Task": "task-example1.json",
+    },
+    "R4B": {},
+    "R5": {},
+}
+
 
 def main() -> None:
     fhir_dir = sys.argv[1]
@@ -17,21 +39,18 @@ def main() -> None:
     with open(resources_json_file) as file_:
         resources = json.load(file_)
 
-    failed = []
     for resource in resources:
-        response = requests.get(
-            f"https://hl7.org/fhir/{sequence}/{resource.lower()}-example.json"
-        )
-        if response.status_code != requests.codes.ok:
-            failed.append(resource)
+        if filename := FILENAME_EXCEPTIONS[sequence].get(resource):
+            url = f"https://hl7.org/fhir/{sequence}/{filename}"
         else:
-            with open(
-                Path(output_dir) / f"{resource.lower()}-example.json", "wb"
-            ) as file_:
-                file_.write(response.content)
+            url = f"https://hl7.org/fhir/{sequence}/{resource.lower()}-example.json"
 
-    if failed:
-        print(f"Failed to obtain examples for: {failed}")
+        response = requests.get(url)
+        if response.status_code != requests.codes.ok:
+            raise RuntimeError(f"Failed to get example for {resource}")
+
+        with open(Path(output_dir) / f"{resource.lower()}-example.json", "wb") as file_:
+            file_.write(response.content)
 
 
 if __name__ == "__main__":
